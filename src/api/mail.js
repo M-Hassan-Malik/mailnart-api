e1137bfb7e78e7391bcf63e32db4488b07b9e9d1
M-Hassan-Malik <mhassan.malik.1997@gmail.com>
2022-04-26 04:13:10 -0800

    Rates API done

--- src/api/mail.js
+++ src/api/mail.js
@@ -1,47 +1,68 @@
-const express = require('express');
+const express = require("express");
 
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-const { GetRatesInput, GetMailToken } = require("../../func/mail")
+const { GetRatesInput, GetMailToken } = require("../../func/mail");
 
 const router = express.Router();
 
-router.post(
-  "/request_rate",
-  GetMailToken,
-  async (req, res, next) => {
-    try {
-      var body = req.body;
-      var FEDEX_ACCOUNT_NUMBER = "510087380";
-      const token = JSON.parse(req.token_res);
-      const access_token = token.access_token;
+router.post("/request_rate", GetMailToken, async (req, res, next) => {
+  try {
+    var body = req.body;
+    var FEDEX_ACCOUNT_NUMBER = "510087380";
+    const token = JSON.parse(req.token_res);
+    const access_token = token.access_token;
 
-       //console.log("TOKEN ===>", token);
+    //console.log("TOKEN ===>", token);
 
-      const input = GetRatesInput(FEDEX_ACCOUNT_NUMBER,body);
+    const input = GetRatesInput(FEDEX_ACCOUNT_NUMBER, body);
 
-      const data = JSON.stringify(input);
-  
-      var xhr = new XMLHttpRequest();
-      xhr.withCredentials = true;
-      
-      xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === 4) {
-          //console.log(this.responseText)
-          res.json(JSON.parse(this.responseText));
+    const data = JSON.stringify(input);
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        //console.log(this.responseText)
+        //res.json(JSON.parse(this.responseText));
+        try {
+          const jsonResponse = JSON.parse(this.responseText);
+
+          res.json({
+            transactionId: jsonResponse.transactionId,
+            rateType:
+              jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
+                .rateType,
+            totalBaseCharge:
+              jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
+                .totalBaseCharge,
+            totalNetCharge:
+              jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
+                .totalNetCharge,
+            totalNetFedExCharge:
+              jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
+                .totalNetFedExCharge,
+            mailnArt_Fee:
+              (10 / 100) *
+              jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
+                .totalNetFedExCharge,
+          });
+        } catch (e) {
+          res.status(400).json({ error: e });
         }
-      });
-      
-      xhr.open("POST", "https://apis-sandbox.fedex.com/rate/v1/rates/quotes");
-      xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.setRequestHeader("X-locale", "en_US");
-      xhr.setRequestHeader("Authorization", `Bearer ${access_token}`);
-      
-      xhr.send(data);
-    } catch (e) {
-      console.log("ERROR trying to call request_rate === ", e);
-    }
+      }
+    });
+
+    xhr.open("POST", "https://apis-sandbox.fedex.com/rate/v1/rates/quotes");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("X-locale", "en_US");
+    xhr.setRequestHeader("Authorization", `Bearer ${access_token}`);
+
+    xhr.send(data);
+  } catch (e) {
+    console.log("ERROR trying to call request_rate === ", e);
   }
-)
+});
 
 module.exports = router;
