0022091f2cfdf72c2335255473ed774c991b1ac8
M-Hassan-Malik <mhassan.malik.1997@gmail.com>
2022-04-26 18:30:56 -0800

    auth and rates - update- 2

--- src/api/mail.js
+++ src/api/mail.js
@@ -26,44 +26,44 @@ router.post("/request_rate", GetMailToken, async (req, res, next) => {
       if (this.readyState === 4) {
         try {
           //console.log(this.responseText)
-        //  res.json(JSON.parse(this.responseText));
-          const jsonResponse = JSON.parse(this.responseText);
+         res.json(JSON.parse(this.responseText));
+          // const jsonResponse = JSON.parse(this.responseText);
 
-          res.json({
-            transactionId: jsonResponse.transactionId,
-            rateType:
-              jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
-                .rateType,
-            totalBaseCharge:
-              jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
-                .totalBaseCharge,
-            totalNetCharge:
-              jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
-                .totalNetCharge,
-            totalNetFedExCharge:
-              jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
-                .totalNetFedExCharge,
-            totalNetFedExCharge:
-              jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
-                .totalNetFedExCharge,
-            mailnArt_Fee:
-              (10 / 100) *
-              jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
-                .totalNetFedExCharge,
-                delivery: {
-              date: jsonResponse.output.rateReplyDetails[0].operationalDetail
-                .deliveryDate,
-              day: jsonResponse.output.rateReplyDetails[0].operationalDetail
-              .deliveryDay,
-            },
-            commits: {
-              commit:  jsonResponse.output.rateReplyDetails[0].commit.dateDetail,
-              date: jsonResponse.output.rateReplyDetails[0].operationalDetail
-                .commitDate,
-              day: jsonResponse.output.rateReplyDetails[0].operationalDetail
-              .commitDays,
-            },
-          });
+          // res.json({
+          //   transactionId: jsonResponse.transactionId,
+          //   rateType:
+          //     jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
+          //       .rateType,
+          //   totalBaseCharge:
+          //     jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
+          //       .totalBaseCharge,
+          //   totalNetCharge:
+          //     jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
+          //       .totalNetCharge,
+          //   totalNetFedExCharge:
+          //     jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
+          //       .totalNetFedExCharge,
+          //   totalNetFedExCharge:
+          //     jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
+          //       .totalNetFedExCharge,
+          //   mailnArt_Fee:
+          //     (10 / 100) *
+          //     jsonResponse.output.rateReplyDetails[0].ratedShipmentDetails[0]
+          //       .totalNetFedExCharge,
+          //       delivery: {
+          //     date: jsonResponse.output.rateReplyDetails[0].operationalDetail
+          //       .deliveryDate,
+          //     day: jsonResponse.output.rateReplyDetails[0].operationalDetail
+          //     .deliveryDay,
+          //   },
+          //   commits: {
+          //     commit:  jsonResponse.output.rateReplyDetails[0].commit.dateDetail,
+          //     date: jsonResponse.output.rateReplyDetails[0].operationalDetail
+          //       .commitDate,
+          //     day: jsonResponse.output.rateReplyDetails[0].operationalDetail
+          //     .commitDays,
+          //   },
+          // });
         } catch (e) {
           res.status(400).json({ error: e });
         }
