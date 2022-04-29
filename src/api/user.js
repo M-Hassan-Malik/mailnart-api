4b572436ba51cb40ff708278cc92cba3362401e2
unknown <mhassan.malik.1997@gmail.com>
2022-04-29 11:09:35 +0500

    get set  order

--- src/api/user.js
+++ src/api/user.js
@@ -10,9 +10,23 @@ router.post("/save_order", (req, res) => {
     .doc(body.transactionId)
     .set(body.order)
     .then((doc) => {
-        doc.writeTime // if exists -> show error
+      doc.writeTime // if exists -> show error
         ? res.status(200).json({ result: doc })
-        : res.status(400).json({ error: doc })
+        : res.status(400).json({ error: doc });
+    });
+});
+
+router.get("/get_orders", (req, res) => {
+  const query = req.query;
+  userRef
+    .doc(query.uid)
+    .collection("orders")
+    .doc(query.transactionId)
+    .get()
+    .then((doc) => {
+      doc.exists // if exists -> show error
+        ? res.status(200).json({ result: doc.data() })
+        : res.status(400).json({ error: "Order not found" });
     });
 });
 
