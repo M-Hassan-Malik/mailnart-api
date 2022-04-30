328468e91d1cb1ce27cc6f05558e236ef6954f53
M-Hassan-Malik <mhassan.malik.1997@gmail.com>
2022-04-30 00:21:48 -0800

    updated HG

--- src/api/payment.js
+++ src/api/payment.js
@@ -1,23 +1,28 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 const { body, validationResult } = require("express-validator");
 
-
-
-console.log('process.env.STRIPE_PRIVATE_KEY -> ',process.env.STRIPE_PUBLISHABLE_KEY)
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
 router.post(
-  "/checkout", 
-  body("items").isArray().notEmpty().withMessage("Items must not be an empty list"),
-  body("items.*.name").isString().withMessage("Each item must contain a valid string property 'name'"),
-  body("items.*.priceInCents").isInt().withMessage("Each item must contain a valid int property 'priceInCents'"),
-  body("items.*.quantity").isInt().withMessage("Each item must contain a valid int property 'quantity'"),
-  // Validator
+  "/checkout",
+  body("items")
+    .isArray()
+    .notEmpty()
+    .withMessage("Items must not be an empty list"),
+  body("items.*.name")
+    .isString()
+    .withMessage("Each item must contain a valid string property 'name'"),
+  body("items.*.priceInCents")
+    .isInt()
+    .withMessage("Each item must contain a valid int property 'priceInCents'"),
+  body("items.*.quantity")
+    .isInt()
+    .withMessage("Each item must contain a valid int property 'quantity'"),
+  // Above Validator
   (req, res, next) => {
-    
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -41,22 +46,22 @@ router.post(
               unit_amount: item.priceInCents,
             },
             quantity: item.quantity,
-          }
+          };
         }),
-        success_url: `https://heuristic-swanson.23-83-37-162.plesk.page/payment-success`,
-        cancel_url: `https://heuristic-swanson.23-83-37-162.plesk.page/payment-cancel`,
-      })
+        success_url: `${process.env.CLIENT_URL}/payment-success`,
+        cancel_url: `${process.env.CLIENT_URL}/payment-cancel`,
+      });
       res.json({
         status: true,
-        url: session.url
-      })
+        url: session.url,
+      });
     } catch (e) {
       res.status(500).json({
         status: false,
-        error: e.message
-      })
+        error: e.message,
+      });
     }
   }
-)
+);
 
 module.exports = router;
