fe38ba302366bc4d06a7b3f42ffdfce317ae1d1f
Syed Ahmed Usman <66121118+syedahmedusman2@users.noreply.github.com>
2022-05-20 17:56:49 +0500

    latest

--- src/api/user.js
+++ src/api/user.js
@@ -4,16 +4,15 @@ const { userRef } = require("../db/ref");
 const admin = require("firebase-admin");
 const db = admin.firestore();
 
-
 router.post("/save_order", (req, res) => {
   const body = req.body;
   userRef
     .doc(body.uid)
     .collection("orders")
-    .doc(body.transactionId)
+    .doc(body.order.transactionId)
     .set({
       ...body.order,
-      status: 'pending'
+      status: "pending",
     })
     .then((doc) => {
       doc.writeTime // if exists -> show error
@@ -33,57 +32,62 @@ router.post("/get_orders", (req, res) => {
       querySnapshot.forEach((doc) => {
         orders.push({
           id: doc.id,
-          ...doc.data()
-        })
-      })
+          ...doc.data(),
+        });
+      });
 
-      res.status(200).json({ result: orders })
-
-    }).catch((err) => {
+      res.status(200).json({ result: orders });
+    })
+    .catch((err) => {
       res.status(400).json({
         status: false,
-        error: err
-      })
-    })
+        error: err,
+      });
+    });
 });
 
 router.post("/edit_orders", (req, res) => {
   const query = req.body;
 
-
-  console.log('body -> ',{
+  console.log("body -> ", {
     uid: query.uid,
-    transactionId: query.transactionId
-  })
-
+    transactionId: query.transactionId,
+  });
 
   try {
     const batch = db.batch();
 
     // Edit Order
-    const ordersRef = userRef.doc(query.uid).collection("orders").doc(query.transactionId)
-    batch.update(ordersRef, { shippingLabel: query.shippingLabel, status: "labeled" }); 
+    const ordersRef = userRef
+      .doc(query.uid)
+      .collection("orders")
+      .doc(query.transactionId);
+    batch.update(ordersRef, {
+      shippingLabel: query.shippingLabel,
+      status: "labeled",
+    });
 
     // Commit the batch
-    batch.commit().then(() => {
-      res.json({
-        status:true,
-        message: "Order Edit Successfull"
+    batch
+      .commit()
+      .then(() => {
+        res.json({
+          status: true,
+          message: "Order Edit Successfull",
+        });
       })
-    }).catch((err)=>{
-      res.json({
-        status:false,
-        error:err
-      })
-    })
+      .catch((err) => {
+        res.json({
+          status: false,
+          error: err,
+        });
+      });
   } catch (error) {
     res.json({
-      status:false,
-      error:error
-    })
+      status: false,
+      error: error,
+    });
   }
-
-
 
   // userRef.doc(query.uid).collection("orders").doc(query.transactionId).update({
   //     shippingLabel: params.shippingLabel
@@ -91,45 +95,49 @@ router.post("/edit_orders", (req, res) => {
   //   res.status(200).json({
   //     status: true,
   //     message: 'success'
-  //   }) 
+  //   })
   // }).catch((err) => {
   //   res.status(400).json({
   //     status: false,
   //     error: err
   //   })
   // })
- 
 });
 
 router.post("/remove_order", (req, res) => {
   const body = req.body;
-  console.log('body -> ',body);
-  
+  console.log("body -> ", body);
 
   try {
     const batch = db.batch();
 
     // Edit Order
-    const ordersRef = userRef.doc(body.uid).collection("orders").doc(body.transactionid)
+    const ordersRef = userRef
+      .doc(body.uid)
+      .collection("orders")
+      .doc(body.transactionid);
     batch.delete(ordersRef);
 
     // Commit the batch
-    batch.commit().then(() => {
-      res.json({
-        status:true,
-        message: "Order Remove Successfull"
+    batch
+      .commit()
+      .then(() => {
+        res.json({
+          status: true,
+          message: "Order Remove Successfull",
+        });
       })
-    }).catch((err)=>{
-      res.json({
-        status:false,
-        error:err
-      })
-    })
+      .catch((err) => {
+        res.json({
+          status: false,
+          error: err,
+        });
+      });
   } catch (error) {
     res.json({
-      status:false,
-      error:error
-    })
+      status: false,
+      error: error,
+    });
   }
 });
 
